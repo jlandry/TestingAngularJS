d89d20fe70a2212bfe4be25953e76f35aee86f84
jlandry <jonamission@gmail.com>
2014-04-02 11:08:22 -0600

    Fun with Gridster/ng-gridster
    Getting a basic grid working in angular.

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var demoApp = angular.module('demoApp', ['ngRoute', 'ui.bootstrap']);
+var demoApp = angular.module('demoApp', ['ngRoute', 'ui.bootstrap', 'ngGridster']);
  
  //basic customer default information and factory to return customers added.
     demoApp.factory('simpleFactory', function() {       
@@ -78,6 +78,23 @@ var demoApp = angular.module('demoApp', ['ngRoute', 'ui.bootstrap']);
             $scope.modalShown = !$scope.modalShown;
             };
     });
+    demoApp.controller('gridster', function($scope){
+            $scope.options = {
+                widget_margins: [10, 10],
+                widget_base_dimensions: [140, 140],
+                helper: 'clone',
+                resize: {
+                  enabled: true,
+                  axes: ['x', 'y', 'both'],
+                  max_size: [3, 1]
+                }
+            };
+    
+            $scope.items = [
+              1,2,3,5,6,7,8,9,10,11,12
+              ];
+          });
+    
     demoApp.controller('MainCtrl', function($scope) {
             $scope.name = 'World';
             $scope.clickData = "tbd";
@@ -170,103 +187,3 @@ demoApp.directive('modalDialog', function() {
   $scope.isCollapsed = false;
   $scope.active = true;
 });
-//Cannot get the d3 to show up.
-//attemted to get a basic directive to show up and cannot. 
-/*.on("click", function(d) {
-config.click(d, d3.event);
-scope.$apply( var renderTimeout;
-          var margin = parseInt(attrs.margin) || 20,
-              barHeight = parseInt(attrs.barHeight) || 20,
-              barPadding = parseInt(attrs.barPadding) || 5;
- 
-          var svg = d3.select(ele[0])
-            .append('svg')
-            .style('width', '100%');
- 
-          $window.onresize = function() {
-            scope.$apply();
-          };
- 
-          scope.$watch(function() {
-            return angular.element($window)[0].innerWidth;
-          }, function() {
-            scope.render(scope.data);
-          });
- 
-          scope.$watch('data', function(newData) {
-            scope.render(newData);
-          }, true);
- 
-          scope.render = function(data) {
-            svg.selectAll('*').remove();
- 
-            if (!data) return;
-            if (renderTimeout) clearTimeout(renderTimeout);
- 
-            renderTimeout = $timeout(function() {
-              var width = d3.select(ele[0])[0][0].offsetWidth - margin,
-                  height = scope.data.length * (barHeight + barPadding),
-                  color = d3.scale.category20(),
-                  xScale = d3.scale.linear()
-                    .domain([0, d3.max(data, function(d) {
-                      return d.score;
-                    })])
-                    .range([0, width]);
- 
-              svg.attr('height', height);
- 
-              svg.selectAll('rect')
-                .data(data)
-                .enter()
-                  .append('rect')
-                  .on('click', function(d,i) {
-                    return scope.onClick({item: d});
-                  })
-                  .attr('height', barHeight)
-                  .attr('width', 140)
-                  .attr('x', Math.round(margin/2))
-                  .attr('y', function(d,i) {
-                    return i * (barHeight + barPadding);
-                  })
-                  .attr('fill', function(d) {
-                    return color(d.score);
-                  })
-                  .transition()
-                    .duration(1000)
-                    .attr('width', function(d) {
-                      return xScale(d.score);
-                    });
-              svg.selectAll('text')
-                .data(data)
-                .enter()
-                  .append('text')
-                  .attr('fill', '#fff')
-                  .attr('y', function(d,i) {
-                    return i * (barHeight + barPadding) + 15;
-                  })
-                  .attr('x', 15)
-                  .text(function(d) {
-                    return d.name + " (scored: " + d.score + ")";
-                  });
-            }, 200););
-});
-angular.module('d3AngularApp', ['d3'])
-.directive('d3Bars', ['$window', '$timeout', 'd3Service', 
-  function($window, $timeout, d3Service) {
-    return {
-      restrict: 'A',
-      scope: {
-        data: '=',
-        label: '@',
-        onClick: '&'
-      },
-
-
-      link: function(scope, ele, attrs) {
-        d3Service.d3().then(function(d3) {
- 
-         
-          };
-        });
-      }}
-})*/
